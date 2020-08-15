d3fde01b612f1268d49f8216ae4f234b69ba5894
Dweep Panchal <dweep.programmer@gmail.com>
2020-08-15 20:21:35 +0530

    database connected

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
+mongoose
+  .connect(process.env.DATABASE, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connected!"))
+  .catch(() => console.log("Something went wrong while connecting DB"));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
